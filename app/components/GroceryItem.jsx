cf04673c0b30e74eba529773cc06cc7630f5e164
harshi93 <singhharshit.4674@gmail.com>
2017-06-06 14:58:08 -0400

    react-express-full-stack-app

---
+++ app/components/GroceryItem.jsx
@@ -0,0 +1,13 @@
+'use strict'
+var React = require('react');
+var Addons = require('react-addons')
+
+module.exports = React.createClass({
+    render: function(){
+        return(
+        <div>
+            <h4 className = {this.props.item.purchased ? "strikethrough":""}>{this.props.item.name}</h4>
+        </div>
+        )
+    } 
+})
---
+++ app/components/GroceryListAddItem.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var Addons = require('react-addons');
+var Actions = require('./../actions/GroceryItemActionCreator.jsx')
+
+module.exports = React.createClass({
+    getInitialState: function(){
+        return {input:""};
+    },
+
+    handleInputName: function(e){
+        this.setState({input: e.target.value});
+    },
+    
+    addItem: function(e){
+        e.preventDefault();
+        Actions.add({
+            name:this.state.input
+        });
+
+        this.setState({
+            input:''
+        })
+    },
+    render:function(){
+        return(
+            <div className ='grocery-addItem'>
+                <form onSubmit={this.addItem}>
+                    <input value={this.state.input} onChange={this.handleInputName}/>
+                    <button>Add Item</button>
+                </form>
+            </div>
+        )
+    }
+})
